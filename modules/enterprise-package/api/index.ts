dc6eca8c58c18e46ec7a4a80d04583d20c7115e4
jiangxing <jiangxing@live.cn>
2026-09-15 16:21:09 +0800

    feat: export enterprise package runtime API

---
+++ modules/enterprise-package/api/index.ts
@@ -0,0 +1,3 @@
+export * from './contracts.js';
+export { validateEnterprisePackage } from '../application/package-validator.js';
+export { planEnterprisePackageDeployment } from '../application/package-planner.js';
